f0de93d48ad47a20d6bd1f1feb1c4ee1da9c3eb4
kamaaTheDj <kamaudavikiruku@gmail.com>
2019-07-18 17:23:28 +0300

    popupMenuButton implementation

--- lib/popUp.dart
+++ lib/popUp.dart
@@ -4,15 +4,11 @@ class Pop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PopupMenuButton(
-      itemBuilder: (BuildContext context) {
-
+      itemBuilder: (BuildContext context) { 
       },
       tooltip: 'meme',
       elevation: 20.0,
-      child: IconButton(
-        onPressed: (){print('klicknalles');},
-        icon: Icon(Icons.more_vert),
-      )
+      icon: Icon(Icons.more_vert),
       );
       
   }

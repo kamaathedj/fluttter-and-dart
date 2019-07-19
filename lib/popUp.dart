8b318c33f2a7503046643693e7e66b62ba34a067
kamaaTheDj <kamaudavikiruku@gmail.com>
2019-07-19 14:44:54 +0300

    :pray: pop up menu is working well , also tried positioning it with LTRB

--- lib/popUp.dart
+++ lib/popUp.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Pop extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return PopupMenuButton(
       itemBuilder: (BuildContext context) { 
+        showMenu(
+          context: context, 
+          items: <PopupMenuEntry>[
+           PopupMenuItem(
+             key: Key('popme'),
+             child: ListTile(leading: Icon(Icons.access_alarm),title: Text('pop me '),),
+             enabled: true,
+             ),
+             PopupMenuDivider(height: 5,),
+             PopupMenuItem(
+               key: Key('settings'),
+               child: ListTile(leading: Icon(Icons.settings),title: Text('Settings'),)
+             )
+          ],
+           position: RelativeRect.fromLTRB(100, 40, 10, 10),
+           
+        );
       },
       tooltip: 'meme',
       elevation: 20.0,
